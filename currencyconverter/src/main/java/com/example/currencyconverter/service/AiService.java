439e7aabe6c70cb71141b7d9ee7ddfad680d3223
Anurag-Codes <anuragsinghyadav2658@gmail.com>
2026-06-12 03:19:06 +0530

    Added AiService file

--- currencyconverter/src/main/java/com/example/currencyconverter/service/AiService.java
+++ currencyconverter/src/main/java/com/example/currencyconverter/service/AiService.java
@@ -3,10 +3,8 @@ package com.example.currencyconverter.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.ObjectMapper;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
