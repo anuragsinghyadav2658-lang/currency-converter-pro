60b3e10202776b1dfff84aec6051077011f17a20
Anurag-Codes <anuragsinghyadav2658@gmail.com>
2026-06-12 17:05:58 +0530

    Added root endpoint for cronjob

--- currencyconverter/src/main/java/com/example/currencyconverter/controller/CurrencyController.java
+++ currencyconverter/src/main/java/com/example/currencyconverter/controller/CurrencyController.java
@@ -51,4 +51,10 @@ public class CurrencyController {
     public List<ConversionRecord> getHistory() {
         return conversionRepository.findAll();
     }
+
+    @GetMapping("/")
+    public String wakeUp() {
+        return "Server is awake and running!";
+    }
+
 }
